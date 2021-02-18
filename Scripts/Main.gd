864e1bdd506db4e7d079ffb04f452890c8a56726
Isrelo <gpoliece@gmail.com>
2021-02-18 05:02:47 -0800

    Started laying out the transaction dynamic list.

---
+++ Scripts/Main.gd
@@ -0,0 +1,6 @@
+extends Control
+
+onready var transactions = $VBoxContainer/Transactions;
+
+func _on_AddTransactionButton_pressed():
+	pass
---
+++ Scripts/TransactionLog.gd
@@ -0,0 +1,5 @@
+extends Control
+
+
+func _on_ScrollContainer_resized():
+	pass # Replace with function body.
