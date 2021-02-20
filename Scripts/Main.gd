8936a69f42825598848600bf8ba72e681f5cf1dc
Isrelo <gpoliece@gmail.com>
2021-02-20 00:04:27 -0800

    Started adding the modal dialog for adding transactions.

--- Scripts/Main.gd
+++ Scripts/Main.gd
@@ -2,13 +2,22 @@ extends Control
 
 onready var total_coins_data_panel = $HBoxContainer/VBoxContainer/HBoxContainer/TotalCoins;
 onready var total_amount_paid_data_panel = $HBoxContainer/VBoxContainer/HBoxContainer/TotalAmountPaid;
+onready var cost_average_data_panel = $HBoxContainer/VBoxContainer/HBoxContainer/CostAverage;
 onready var transaction_log = $HBoxContainer/VBoxContainer/TransactionLog;
+onready var add_trans_dialog = $AddTransaction;
 
 # User wants to add a new transaction.
 func _on_TransactionLog_AddTransactionClick():
+	# Step 1: Show the add transaction dialog.
+	add_trans_dialog.fade_in();
+	
+	# Step 2: Add the transaction to the transaction log.
 	transaction_log.add_transaction();
+	
+	# Step 3: Recalulate the displayed totals.
 	transaction_log.calcualte_total_coins();
 	transaction_log.calculate_total_price();
+	transaction_log.calcualte_cost_average();
 
 # User wants to calcualte a sell.
 func _on_TransactionLog_CalcualteSellClick():
@@ -23,3 +32,8 @@ func _on_TransactionLog_CalculatedNumberOfCoins(number_of_coins_p):
 func _on_TransactionLog_CalcualteTotalPrice(amount_paid_p):
 	if total_amount_paid_data_panel != null:
 		total_amount_paid_data_panel.set_data(String("$" + "%3.2f" % amount_paid_p));
+
+# Cost average was calcualted. Refresh your data.
+func _on_TransactionLog_CalculateCostAverage(cost_average_p):
+	if cost_average_data_panel != null:
+		cost_average_data_panel.set_data(String("$" + "%3.2f" % cost_average_p));
--- Scripts/TransactionLog.gd
+++ Scripts/TransactionLog.gd
@@ -4,6 +4,7 @@ signal AddTransactionClick;
 signal CalcualteSellClick;
 signal CalculatedNumberOfCoins;
 signal CalcualteTotalPrice;
+signal CalculateCostAverage;
 
 onready var transaction_list = $MarginContainer/VBoxContainer/ScrollContainer/Trasactions;
 
@@ -33,24 +34,37 @@ func calculate_total_price():
 			total_price -= transaction_view.get_amount_paid();
 	emit_signal("CalcualteTotalPrice", total_price);
 
+# Calcualte the cost average of the purchased coins.
+func calcualte_cost_average():
+	# Get all of the price transactions.
+	var total_price : float = 0.0;
+	var coin_count : float = 0.0;
+	var transaction_views = get_tree().get_nodes_in_group("Transaction");
+	for transaction_view in transaction_views:
+		if transaction_view.is_credit_trans():
+			total_price += transaction_view.get_amount_paid();
+			coin_count += transaction_view.get_number_of_coins();
+		else:
+			total_price -= transaction_view.get_amount_paid();
+			coin_count -= transaction_view.get_number_of_coins();
+	var cost_average = (total_price / coin_count);
+	emit_signal("CalculateCostAverage", cost_average);
+
 func add_transaction():
 	var temp_trans_view = transaction_view.instance();
 	transaction_list.add_child(temp_trans_view);
 	
-	# date_acquired_p
-	# number_of_coins_p
-	# exchange_price_p
-	# credit_amount_p
-	# debit_amount_p
+	# Trade 1
 	var temp_class : Transaction;
 	temp_class = load("res://Scripts/Autoload/Transaction.gd").new();
-	temp_class.set_trans_data("02/17/2021", 0.001920220, 52077.37, 100.00, true);
+	temp_class.set_trans_data("02/17/2021", 0.001932000, 51759.83, 100.00, true);
 	temp_trans_view.set_tras_data(temp_class);
 	
+	# Trade 2
 	temp_trans_view = transaction_view.instance();
 	transaction_list.add_child(temp_trans_view);
 	temp_class = load("res://Scripts/Autoload/Transaction.gd").new();
-	temp_class.set_trans_data("02/17/2021", 0.000020220, 52859.43, 75.00, false);
+	temp_class.set_trans_data("02/17/2021", 0.001928520, 51853.23, 100, true);
 	temp_trans_view.set_tras_data(temp_class);
 
 # User wants to add a new transaction.
