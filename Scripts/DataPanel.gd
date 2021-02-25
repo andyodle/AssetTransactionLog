95349dfd464c337d5f1dccade394421f601d256a
Isrelo <gpoliece@gmail.com>
2021-02-24 20:18:04 -0800

    Started figuring out how the sell transaction dialog will work. Worked on getting the sell transaction dialog to calcuate the current profits.

--- Scripts/DataPanel.gd
+++ Scripts/DataPanel.gd
@@ -3,4 +3,15 @@ extends Control
 onready var data = $MarginContainer/CenterContainer/VBoxContainer/Data;
 
 func set_data(data_p):
+	var temp_data : int = round(float(data_p.replace("$", "")));
+	if int(temp_data) >= 0:
+		# Gree for positive profit.
+		data.set("custom_colors/font_color", Color(0,255,0,255));
+	else:
+		# Red for nagative profit.
+		data.set("custom_colors/font_color", Color(255,0,0,255));
 	data.text = String(data_p);
+
+func get_data():
+	var temp_data = data.text.replace("$", "");
+	return temp_data;
--- Scripts/SellTransactionDialog.gd
+++ Scripts/SellTransactionDialog.gd
@@ -6,6 +6,7 @@ onready var number_of_coins = $CenterContainer/DialogContainer/MarginContainer/C
 onready var total_coins_data_panel = $CenterContainer/DialogContainer/MarginContainer/CenterContainer/VBoxContainer/HBoxContainer/TotalCoins
 onready var total_amount_paid_data_panel = $CenterContainer/DialogContainer/MarginContainer/CenterContainer/VBoxContainer/HBoxContainer/TotalAmountPaid;
 onready var cost_average_data_panel = $CenterContainer/DialogContainer/MarginContainer/CenterContainer/VBoxContainer/HBoxContainer/CostAverage;
+onready var total_profit_data_panel = $CenterContainer/DialogContainer/MarginContainer/CenterContainer/VBoxContainer/HBoxContainer/TotalProfit;
 onready var transaction_log = $CenterContainer/DialogContainer/MarginContainer/CenterContainer/VBoxContainer/TransactionLog;
 
 func reset_dialog():
@@ -59,3 +60,40 @@ func _on_DialogActionButtons_OkClicked():
 # Cancel adding a sell transaction.
 func _on_DialogActionButtons_CancelClicked():
 	self.fade_out();
+
+func calculate_total_gain():
+	# Purchased Data
+	var purchased_total_price : float;
+	purchased_total_price = float(total_amount_paid_data_panel.get_data());
+	var purchased_number_of_coins : float;
+	purchased_number_of_coins = float(total_coins_data_panel.get_data());
+	var purchased_cost_average : float;
+	purchased_cost_average = float(cost_average_data_panel.get_data());
+	
+	# Sold Data
+	var sold_exchange_rate : float;
+	var sold_number_of_coins : float = float(number_of_coins.text);
+	
+	# Left Over Data
+	var total_gains : float;
+	var left_over_number_of_coins : float;
+	var left_over_exchange_rate : float;
+	
+	# Make sure we have some coins.
+	if sold_number_of_coins != 0:
+		sold_exchange_rate = purchased_total_price / sold_number_of_coins;
+		
+		# Calcualte Possible Gains
+		left_over_number_of_coins = purchased_number_of_coins - sold_number_of_coins;
+		left_over_exchange_rate = sold_exchange_rate;
+		total_gains = left_over_number_of_coins * left_over_exchange_rate;
+		
+		# Display the total gains.
+		total_profit_data_panel.set_data(String("$" + "%3.2f" % total_gains));
+
+# User entered a number recalulate the possilbe gains.
+func _on_NumberOfCoins_text_changed():
+	# Validate the input text.
+	# Calculate the total possible gain.
+	calculate_total_gain();
+	pass
