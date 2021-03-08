64955ca516668812346d49ee9884de354b38233e
Isrelo <gpoliece@gmail.com>
2021-03-08 03:58:56 -0800

    Fixing calulation bug.

--- Development/Scripts/AutoLoad/Utility.gd
+++ Development/Scripts/AutoLoad/Utility.gd
@@ -14,44 +14,36 @@ func calcualte_total_coins(transacion_list_p):
 	var transaction_views = transacion_list_p.get_children();
 	for transaction_view in transaction_views:
 		var trans_data = transaction_view.trans_data;
-#		if trans_data.is_credit_m:
 		coin_count = calculator.add(coin_count, trans_data.number_of_coins_m);
-#		else:
-#			coin_count = calculator.subtract(coin_count, trans_data.number_of_coins_m);
 	
 	return coin_count;
 
 # Calcualte the total price paid for the coins.
 func calculate_total_price(transacion_list_p):
+	var calculator = Calculator.new();
 	# Get all of the price transactions.
-	var total_price : float = 0.0;
+	var total_price : String = "0.0";
 	var transaction_views = transacion_list_p.get_children();
 	for transaction_view in transaction_views:
 		var trans_data = transaction_view.trans_data;
-		if trans_data.is_credit_m:
-			total_price += float(trans_data.amount_m);
-		else:
-			total_price -= float(trans_data.amount_m);
+		total_price = calculator.add(total_price, trans_data.amount_m);
 	
 	return total_price;
 
 # Calcualte the cost average of the purchased coins.
 func calcualte_cost_average(transacion_list_p):
+	var calculator = Calculator.new();
 	# Get all of the price transactions.
-	var total_price : float = 0.0;
-	var coin_count : float = 0.0;
-	var cost_average : float = 0.0;
+	var total_price : String = "0.0";
+	var coin_count : String = "0.0";
+	var cost_average : String = "0.0";
 	var transaction_views = transacion_list_p.get_children();
 	for transaction_view in transaction_views:
 		var trans_data = transaction_view.trans_data;
-		if trans_data.is_credit_m:
-			total_price += float(trans_data.amount_m);
-			coin_count += float(trans_data.number_of_coins_m);
-		else:
-			total_price -= float(trans_data.amount_m);
-			coin_count -= float(trans_data.number_of_coins_m);
-	if coin_count != 0:
-		cost_average = (total_price / coin_count);
+		total_price = calculator.add(total_price, trans_data.amount_m);
+		coin_count = calculator.add(coin_count, trans_data.exchange_price_m);
+	if float(coin_count) != 0:
+		cost_average = calculator.divide(total_price, coin_count);
 	
 	return cost_average;
 
