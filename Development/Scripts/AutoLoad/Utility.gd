b61d3da5d2c0aec2767c5dfa9c8d60f2f6f66e74
Isrelo <gpoliece@gmail.com>
2021-03-11 17:28:00 -0800

    Fixed a bug with the date being in dd/mm/yyyy instead of mm/dd/yyyy. Updated trading positions.

--- Development/Scripts/AutoLoad/Utility.gd
+++ Development/Scripts/AutoLoad/Utility.gd
@@ -3,7 +3,7 @@ extends Node
 # Get the current date.
 func get_current_date_str():
 	var temp_date_time = OS.get_datetime();
-	var date_string = String("%02d" % temp_date_time["day"]) + "/" + String("%02d" % temp_date_time["month"]) + "/" + String("%4d" % temp_date_time["year"]);
+	var date_string = String("%02d" % temp_date_time["month"]) + "/" + String("%02d" % temp_date_time["day"]) + "/" + String("%4d" % temp_date_time["year"]);
 	return date_string;
 
 # Calcualte the total number of coins acquired.
