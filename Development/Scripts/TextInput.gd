0a23e06f0d96fddcb4bed9186a97026bb31beb1d
Isrelo <gpoliece@gmail.com>
2021-03-17 17:19:27 -0700

    Adjusting the font spacing to make it easier to read.

--- Development/Scripts/TextInput.gd
+++ Development/Scripts/TextInput.gd
@@ -16,3 +16,10 @@ func _on_TextInput_focus_exited():
 			animation_player.play("GrowLabel");
 			yield(animation_player, "animation_finished");
 			has_focus = false;
+
+func _input(event):
+	# Check to make sure tab doesn't print in the TextEdit.
+	if event.is_action_pressed("ui_focus_next") and has_focus():
+		if focus_next != "":
+			get_node(focus_next).grab_focus()
+		get_tree().set_input_as_handled()
