dcee280ea6e9f6624e1e9a99cf969d25cc6d3c18
Isrelo <gpoliece@gmail.com>
2021-06-28 23:53:57 -0700

    Working on adding split transaction functionality.

--- Development/Scripts/SplitTransactionDialog.gd
+++ Development/Scripts/SplitTransactionDialog.gd
@@ -1,31 +1,58 @@
 extends "res://Scripts/DialogWindow.gd"
 
+onready var reduce_transaction_input = $CenterContainer/DialogContainer/MarginContainer/CenterContainer/VBoxContainer/ReduceTransactionInput;
+onready var transaction_log = $CenterContainer/DialogContainer/MarginContainer/CenterContainer/VBoxContainer/TransactionLog;
+
 signal SplitTransaction;
 
+var reduced_transaction;
+
 func reset_dialog():
-	pass;
+	reduce_transaction_input.text = "";
+	transaction_log.reset_trasactoins();
 
 func verify_input():
 	return true;
+
+# Add selected transactions to the split log.
+func add_transaction_to_split(var transactoin_to_split_p):
+	transaction_log.add_transaction(transactoin_to_split_p, true);
+
+func calculate_split_transaction():
+	# Double Custom Calculator
+	var calculator = Calculator.new();
+	
+	# Get the amount to reduce the transaction by.
+	var amount_to_reduce = reduce_transaction_input.text;
+	
+	# Make sure we have an amount to calcualte.
+	if float(amount_to_reduce) != 0:
+		var transaction_views = transaction_log.get_transactions();
+		for transaction_view in transaction_views:
+			var temp_class : Transaction;
+			temp_class = load("res://Scripts/Classes/Transaction.gd").new();
+			temp_class.date_m = Utility.get_current_date_str();
+			temp_class.number_of_coins_m = calculator.divide(amount_to_reduce, transaction_view.trans_data.exchange_price_m).pad_decimals(8);
+			temp_class.exchange_price_m = transaction_view.trans_data.exchange_price_m;
+			temp_class.amount_m = amount_to_reduce;
+			temp_class.is_credit_m = true;
+			reduced_transaction = temp_class;
+	
 
 # User clicked OK
 func _on_DialogActionButtons_OkClicked():
 	# Step1: Validate User Input.
 	if verify_input():
-		# Step2: Create and fillout a Transaction obect.
-		var temp_class : Transaction;
-		temp_class = load("res://Scripts/Classes/Transaction.gd").new();
-		#temp_class.date_m = transaction_date.text;
-		#temp_class.number_of_coins_m = number_of_coins.text;
-		#temp_class.exchange_price_m = exchange_price.text;
-		#temp_class.amount_m = transaction_ammount.text;
-		#temp_class.is_credit_m = credit_or_debit.get_checked();
-		
-		# Step3: Emit signal with transaction data.
-		emit_signal("SplitTransaction", temp_class);
+		if reduced_transaction != null:
+			# Step2: Emit signal with transaction data.
+			emit_signal("SplitTransaction", reduced_transaction);
 		
 		self.fade_out();
 
 # User clicked Cancel
 func _on_DialogActionButtons_CancelClicked():
 	self.fade_out();
+
+# User enterd an amount.
+func _on_ReduceTransactionInput_text_changed():
+	calculate_split_transaction();
