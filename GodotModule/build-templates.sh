7ae7be40589567f7397ccf1cc2b08d06c012ae32
Isrelo <gpoliece@gmail.com>
2024-12-21 17:41:32 -0800

    Worked on creating script to automaticly build and rename template scripts.

--- GodotModule/build-templates.sh
+++ GodotModule/build-templates.sh
@@ -1,7 +1,26 @@
-#!/bin/bash
+#! /bin/bash
+set -x #echo on
 
 # godot engine version number
-godot_engine_version_num = "4.2.2-stable"
+godot_engine_ver_num="4.2.2-stable"
+# Path to godot engine source code.
+godot_engine_source_path="./GodotEngineSource/godot-$godot_engine_ver_num"
 
 # Switch into godot code folder.
-cd "./godot-$godot_engine_version_num"
+cd $godot_engine_source_path
+
+# Create Tempalte Output Directory
+mkdir -p ../../Template/$godot_engine_ver_num
+
+# Compile 64 bit Linux Debug
+scons platform=linuxbsd target=template_debug arch=x86_64
+
+# Move target build into template directory.
+mv ./bin/godot.linuxbsd.template_debug.x86_64 "../../Template/$godot_engine_ver_num/linux_debug.x86_64"
+
+# Compile 64 bit Linux Release
+scons platform=linuxbsd target=template_release arch=x86_64
+
+# Move target build into template directory.
+mv ./bin/godot.linuxbsd.template_release.x86_64 "../../Template/$godot_engine_ver_num/linux_release.x86_64"
+
