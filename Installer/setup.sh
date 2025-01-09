3998af0a4ec3a262ba4b640abf5fd14736873a44
Isrelo <gpoliece@gmail.com>
2025-01-08 18:52:18 -0800

    Updated to v0.1.1

--- Installer/setup.sh
+++ Installer/setup.sh
@@ -4,10 +4,27 @@ set -x #echo on
 # AssetTransactionLog variables
 atl_app_name=AssetTransactionLog.x86_64
 
-# Update the .desktop file.
-echo "Name=Asset Transaction Log" >> ./AssetTransacitonLog.desktop
-echo "Exec=$HOME/.AssetTransactionLog/$atl_app_name" >> ./AssetTransacitonLog.desktop
-echo "Icon=$HOME/.AssetTransactionLog/Icon.png" >> ./AssetTransacitonLog.desktop
+# Update the .desktop file Name setting.
+if grep -q "Name=Asset Transaction Log" ./AssetTransacitonLog.desktop; then
+	echo ".desktop NAME already set."
+else
+	# Update the .desktop file.
+	echo "Name=Asset Transaction Log" >> ./AssetTransacitonLog.desktop
+fi
+
+# Update the .desktop file Exec setting.
+if grep -q "Exec=$HOME/.AssetTransactionLog/$atl_app_name" ./AssetTransacitonLog.desktop; then
+	echo ".desktop Exec already set."
+else	
+	echo "Exec=$HOME/.AssetTransactionLog/$atl_app_name" >> ./AssetTransacitonLog.desktop
+fi
+
+# Update the .desktop file Icon setting.
+if grep -q "Icon=$HOME/.AssetTransactionLog/Icon.png" ./AssetTransacitonLog.desktop; then
+	echo ".desktop Icon already set."
+else
+	echo "Icon=$HOME/.AssetTransactionLog/Icon.png" >> ./AssetTransacitonLog.desktop
+fi
 
 # Make directory to store ATL files.
 mkdir -p ~/.AssetTransactionLog/
