aa704b3495dc5fd5542363fac8c82f5390252910
Isrelo <gpoliece@gmail.com>
2025-01-09 11:47:03 -0800

    Updaded godot-engine template build script to include 64bit windows.

--- GodotModule/build-templates-linux.sh
+++ GodotModule/build-templates-linux.sh
@@ -24,3 +24,24 @@ scons platform=linuxbsd target=template_release arch=x86_64
 # Move target build into template directory.
 mv ./bin/godot.linuxbsd.template_release.x86_64 "../../Template/$godot_engine_ver_num/linux_release.x86_64"
 
+# Compile 64 bit Windows Debug
+scons platform=windows target=template_debug arch=x86_64
+
+# Move target build into template directory.
+mv ./bin/godot.windows.template_debug.x86_64.console.exe "../../Template/$godot_engine_ver_num/windows_debug_x86_64_console.exe"
+mv ./bin/godot.windows.template_debug.x86_64.exe "../../Template/$godot_engine_ver_num/windows_debug_x86_64.exe"
+
+# Compile 64 bit Windows release
+scons platform=windows target=template_release arch=x86_64
+
+# Move target build into template directory.
+mv ./bin/godot.windows.template_release.x86_64.console.exe "../../Template/$godot_engine_ver_num/windows_release_x86_64_console.exe"
+mv ./bin/godot.windows.template_release.x86_64.exe "../../Template/$godot_engine_ver_num/windows_release_x86_64.exe"
+
+
+
+
+
+
+
+
