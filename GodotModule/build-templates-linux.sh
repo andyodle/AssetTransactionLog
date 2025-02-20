416307329b171aec8193931468553a5eccc482d6
Isrelo <isrelo@blacklightcoders.com>
2025-02-20 01:21:57 -0800

    Cleaning up project code in order to prepare for posting to github.

--- GodotModule/build-templates-linux.sh
+++ GodotModule/build-templates-linux.sh
@@ -2,46 +2,41 @@
 set -x #echo on
 
 # godot engine version number
-godot_engine_ver_num="4.2.2-stable"
+godot_engine_ver_num="$1-stable"
 # Path to godot engine source code.
-godot_engine_source_path="./GodotEngineSource/godot-$godot_engine_ver_num"
+godot_engine_source_path="godot-$godot_engine_ver_num"
 
 # Switch into godot code folder.
-cd $godot_engine_source_path
+cd ./$pwd/GodotEngineSource/$godot_engine_source_path/
 
 # Create Tempalte Output Directory
-mkdir -p ../../Template/$godot_engine_ver_num
+mkdir -p ../Template/$godot_engine_ver_num
 
 # Compile 64 bit Linux Debug
 scons platform=linuxbsd target=template_debug arch=x86_64
 
 # Move target build into template directory.
-mv ./bin/godot.linuxbsd.template_debug.x86_64 "../../Template/$godot_engine_ver_num/linux_debug.x86_64"
+mv ./bin/godot.linuxbsd.template_debug.x86_64 "../Template/$godot_engine_ver_num/linux_debug.x86_64"
 
 # Compile 64 bit Linux Release
 scons platform=linuxbsd target=template_release arch=x86_64
 
 # Move target build into template directory.
-mv ./bin/godot.linuxbsd.template_release.x86_64 "../../Template/$godot_engine_ver_num/linux_release.x86_64"
+mv ./bin/godot.linuxbsd.template_release.x86_64 "../Template/$godot_engine_ver_num/linux_release.x86_64"
 
 # Compile 64 bit Windows Debug
 scons platform=windows target=template_debug arch=x86_64
 
 # Move target build into template directory.
-mv ./bin/godot.windows.template_debug.x86_64.console.exe "../../Template/$godot_engine_ver_num/windows_debug_x86_64_console.exe"
-mv ./bin/godot.windows.template_debug.x86_64.exe "../../Template/$godot_engine_ver_num/windows_debug_x86_64.exe"
+mv ./bin/godot.windows.template_debug.x86_64.console.exe "../Template/$godot_engine_ver_num/windows_debug_x86_64_console.exe"
+mv ./bin/godot.windows.template_debug.x86_64.exe "../Template/$godot_engine_ver_num/windows_debug_x86_64.exe"
 
 # Compile 64 bit Windows release
 scons platform=windows target=template_release arch=x86_64
 
 # Move target build into template directory.
-mv ./bin/godot.windows.template_release.x86_64.console.exe "../../Template/$godot_engine_ver_num/windows_release_x86_64_console.exe"
-mv ./bin/godot.windows.template_release.x86_64.exe "../../Template/$godot_engine_ver_num/windows_release_x86_64.exe"
-
-
-
-
-
+mv ./bin/godot.windows.template_release.x86_64.console.exe "../Template/$godot_engine_ver_num/windows_release_x86_64_console.exe"
+mv ./bin/godot.windows.template_release.x86_64.exe "../Template/$godot_engine_ver_num/windows_release_x86_64.exe"
 
 
 
---
+++ SetupGodotEngine.sh
@@ -0,0 +1,43 @@
+#! /bin/bash
+#set -x #echo on
+
+godot_engine_version=$1
+godot_source_path="godot-$godot_engine_version-stable"
+
+# Download the godot engine source code.
+#wget -O ./GodotEngineSource/$godot_engine_version-stable.zip https://github.com/godotengine/godot/archive/refs/tags/$godot_engine_version-stable.zip
+
+# Extract godot engine source code.
+unzip -o ./GodotEngineSource/$godot_engine_version-stable.zip -d "./GodotEngineSource/"
+
+# Copy calculator module into engine source code dir.
+cp -rf ./GodotModule/calculator ./GodotEngineSource/$godot_source_path/modules/calculator
+
+# Copy template install script into engine source code dir.
+cp -rf ./GodotModule/build-templates-linux.sh ./GodotEngineSource/build-templates-linux.sh
+
+# Switch to GodotEngineSource dir.
+cd ./GodotEngineSource/$godot_source_path/
+
+# List available platforms.
+echo ''
+temp_list=`scons platform=list`
+green='\033[0;32m'
+white='\033[0;32m'
+echo -e "$green$temp_list$white"
+
+# Get witch platform to compile.
+read -p "Witch platform will you be developing on? " dev_platform
+
+# Compile godot engine for developer platform.
+scons platform=$dev_platform
+
+# Switch to script dir.
+cd ../../
+
+# Compile Target Platform Templates
+./GodotEngineSource/build-templates-linux.sh $godot_engine_version
+
+# Create Release Folders.
+mkdir -p Release/Windows
+mkdir -p Release/Linux
